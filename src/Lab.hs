2fd8c32878fed287ed1b8dc555a1a0a151a8733c
Miklos Hamar <Miklos.Hamar@warwick.ac.uk>
2020-01-29 21:16:35 +0000

    immense amount of progress finally

--- src/Lab.hs
+++ src/Lab.hs
@@ -19,23 +19,27 @@ import Prelude hiding ( elem, maximum, intersperse, transpose
 
 elem :: Eq a => a -> [a] -> Bool
 elem p [] = False
-elem p (x:xs) = not (null (filter (== p) (x:xs)))
+elem p x = any (== p) x
 
+--elem' p = foldr (\x -> x == p) False
 
 maximum :: Ord a => [a] -> a
-maximum = undefined
+maximum = foldr1 f
+  where f y z = if y>z then y else z
 
 any :: (a -> Bool) -> [a] -> Bool
-any = undefined
+any p [] = False
+any p (x:y) = p x || any p y
 
 all :: (a -> Bool) -> [a] -> Bool
-all = undefined
+all p [] = True
+all p (x:y) = p x && all p y
 
 flip :: (a -> b -> c) -> b -> a -> c
-flip = undefined
+flip f x y = f y x
 
 takeWhile :: (a -> Bool) -> [a] -> [a]
-takeWhile = undefined
+takeWhile f (x:xs) = if not (f x) then [] else x : takeWhile f xs
 
 zipWith :: (a -> b -> c) -> [a] -> [b] -> [c]
 zipWith = undefined
